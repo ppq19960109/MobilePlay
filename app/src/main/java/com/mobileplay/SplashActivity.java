4cc4e1abd781e80faf7ee82947652c067e4b36a9
ppq19960109 <pangpengqiang@163.com>
2019-09-14 21:02:41 +0800

    111

--- app/src/main/java/com/mobileplay/SplashActivity.java
+++ app/src/main/java/com/mobileplay/SplashActivity.java
@@ -11,6 +11,6 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         123
-
+        setContentView(R.layout.activity_splash);
     }
 }
