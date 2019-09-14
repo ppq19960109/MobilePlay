36bdc113323367fed1b8a82ffc1d4deae6f8e3ed
ppq19960109 <pangpengqiang@163.com>
2019-09-14 23:18:03 +0800

    main9.14

---
+++ app/src/main/java/com/mobileplay/activity/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.mobileplay.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+
+import com.mobileplay.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler=new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        startMainActivity();
+        return super.onTouchEvent(event);
+    }
+}
