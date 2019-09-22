7425b8fb582de803b4fb14fb8f2ace9a413fe34c
ppq19960109 <pangpengqiang@163.com>
2019-09-22 18:27:37 +0800

    9

--- app/src/main/java/com/mobileplay/pager/VideoPager.java
+++ app/src/main/java/com/mobileplay/pager/VideoPager.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -125,5 +126,27 @@ public class VideoPager extends BasePager {
         tv_nomedia = view.findViewById(R.id.tv_nomedia);
         pb_load = view.findViewById(R.id.pb_load);
         initData();
+    }
+    class VideoAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
